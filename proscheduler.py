cdb6b23c5bcf767f9cff44f89699656123183363
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2023-10-22 11:53:05 +0500

    minor edits

--- proscheduler.py
+++ proscheduler.py
@@ -42,7 +42,6 @@ search_input.send_keys("Lahore, Pakistan")
 search_button = driver.find_element(By.ID, "btnSearch")
 search_button.click()
 
-# Find the link element and click it
 # selecting center
 availability_link = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//a[@title='Availability - 8783:LAHORE, PAKISTAN#8783']"))
@@ -56,8 +55,10 @@ dropdown_month = WebDriverWait(driver, 10).until(
 
 
 # Read user input from a text file
+'''
 with open('month_year.txt', 'r') as file:
     user_input = file.read().strip()
+'''
 
 # Select the option with the value user_input i.e "12 2023"
 # select month and year.......................................
@@ -70,15 +71,12 @@ submit_button.click()
 
 
 #check for active links... Availability
-# Wait for all elements with class="calActiveLink" to be clickable
 active_links = WebDriverWait(driver, 10).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "calActiveLink"))
 )
 
 if active_links:
-    # Print the values
     for link in active_links:
         print(link.text)
 else:
-    # No active links found
     print("Not available!!")
