b5c76067bba83cdbd0610732afe96e200877e635
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2024-02-22 10:54:07 +0500

    version 1.3

--- proscheduler.py
+++ proscheduler.py
@@ -182,7 +182,7 @@ class CronJobs:
     def create_job(self, operating_system, schedule, script_path, script_name):
         if operating_system == "Linux":
             # Find the display value
-            display_info = subprocess.check_output(["echo", "$DISPLAY"]).decode().strip()
+            display = subprocess.check_output(["echo", "$DISPLAY"]).decode().strip()
 
             # command for setting up cronjob
             command = f'(crontab -l; echo "{schedule} export DISPLAY={display} cd {script_path} && python3 {script_name} >> {script_path}/logfile 2>&1") | crontab -'
