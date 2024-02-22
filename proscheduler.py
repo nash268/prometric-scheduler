df74a7b0df758973203f55f055b593b99ec2e26c
Nesh Kumar <130772656+nash268@users.noreply.github.com>
2024-02-22 18:30:58 +0500

    version 1.3

--- proscheduler.py
+++ proscheduler.py
@@ -45,6 +45,42 @@ print("Let's get started!")
 print("------------------------------------------------")
 
 
+# usage examples of sanitised_input():
+# age = sanitised_input("Enter your age: ", int, 1, 101)
+# answer = sanitised_input("Enter your answer: ", str.lower, range_=('a', 'b', 'c', 'd'))
+def sanitised_input(prompt, type_=None, min_=None, max_=None, range_=None):
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    expected = " or ".join((
+                        ", ".join(str(x) for x in range_[:-1]),
+                        str(range_[-1])
+                    ))
+                    print(template.format(expected))
+        else:
+            return ui
+
+
 # Check if it's the first time running the script
 try:
     with open("user_input.txt", "r") as file:
@@ -53,8 +89,8 @@ try:
         print("Values loaded from previous session file user_input.txt.")
 except FileNotFoundError:
     # If file doesn't exist, prompt user for input
-    exam_name = input("Enter exam name (STEP1/STEP2): ")
-    month_year = input("Enter month and year (3 2024): ")
+    exam_name = sanitised_input("Enter exam name (STEP1/STEP2): ", str.upper, range_=('STEP1', 'STEP2'))
+    month_year = sanitised_input("Enter month and year (3 2024): ", str)
 
     # Display available cities to the user and prompt for selection
     print("Available cities:")
@@ -62,15 +98,15 @@ except FileNotFoundError:
         print(f"{index}. {city}")
 
     # input for cities
-    selected_city_indices = input("Enter the numbers corresponding to the cities you want to check (separated by space): ") or '1 2'
+    selected_city_indices = sanitised_input("Enter the numbers corresponding to the cities you want to check (separated by space): ", str, range_=('1', '2', '1 2')) or '1 2'
 
-    start_date = input("Enter start date (1): ")
-    end_date = input("Enter end date (31): ")
+    start_date = sanitised_input("Enter start date (1): ", int, 1, 31)
+    end_date = sanitised_input("Enter end date (31): ", int, 1, 31)
 
     if (operating_system == "Linux" or operating_system == "Darwin"):
         print("")
         print("\033[33mWarning! This will delete all other cronjobs\033[0m")
-        schedule_task = input("Schedule script to run automatically(yes/no): ")    
+        schedule_task = sanitised_input("Schedule script to run automatically(yes/no): ", str.lower, range_=('yes', 'no'))    
         if schedule_task == "yes":
             print("")
             print("------------------------------------------------------------")
@@ -80,12 +116,12 @@ except FileNotFoundError:
             schedule = input("how frequently you want to run script?: ")
 
     if (operating_system == "Windows"):
-        schedule_task = input("Schedule script to run automatically(yes/no): ")
+        schedule_task = sanitised_input("Schedule script to run automatically(yes/no): ", str.lower, range_=('yes', 'no'))
         if schedule_task == "yes":
             print("")
             print("------------------------------------------------------")
             print("for example, To run script after every 30 minutes input 30")
-            schedule = input("Input number of minutes (30): ")
+            schedule = sanitised_input("Input number of minutes (30): ", int, 5)
 
 
     # Write input values to file for later use
